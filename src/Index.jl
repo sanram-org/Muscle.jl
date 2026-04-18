be9571ef0ffc08749cf53feefd16836b4442996c
Sergio Sanchez Ramirez <15837247+mofeing@users.noreply.github.com>
2026-04-18 21:24:57 +0200

    add covariance information to index

--- src/Index.jl
+++ src/Index.jl
@@ -1,16 +1,54 @@
-struct Index{T}
-    tag::T
+"""
+    Label{T}
+
+Represents the label of an [`Index`](@ref); i.e. the matching name without variance information.
+"""
+struct Label{T}
+    label::T
 end
+
+Label(name::String) = Label(Symbol(name))
+
+"""
+    Variance
+
+Variance type of an index. It can be `Covariant`, `Contravariant` or `Invariant`.
+"""
+@enum Variance begin
+    Covariant
+    Contravariant
+    Invariant # NOTE do not use in principle
+end
+
+"""
+    Index{T}
+
+Represents an index of a tensor. It consists of a [`Label`](@ref) and [`Variance`](@ref) information.
+"""
+struct Index{T}
+    label::T
+    variance::Variance
+end
+
+Index(ind::Index, variance::Variance = variance(ind)) = Index(label(ind), variance)
 
 # TODO checkout whether this is a good idea
 Base.copy(x::Index) = x
 
-Index(ind::Index) = ind
-Index(name::String) = Index(Symbol(name))
+label(ind::Index) = ind.label
+variance(ind::Index) = ind.variance
+
+is_equal_label(ind::Index) = Base.Fix1(is_equal_label, ind)
+is_equal_label(ind1::Index, ind2::Index) = label(ind1) == label(ind2)
 
 function Base.show(io::IO, ind::Index)
     print(io, "index<")
-    print(io, ind.tag)
+    print(io, label(ind))
+    if variance(ind) == Contravariant
+        print(io, "↑")
+    elseif variance(ind) == Covariant
+        print(io, "↓")
+    end
     print(io, ">")
 end
 
