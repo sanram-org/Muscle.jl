e7eb6222550a53f9d3c69ac0575c365460f657f2
Sergio Sanchez Ramirez <15837247+mofeing@users.noreply.github.com>
2026-04-19 12:19:15 +0200

    fix `Index`, `Label` constructors

--- src/Index.jl
+++ src/Index.jl
@@ -5,9 +5,14 @@ Represents the label of an [`Index`](@ref); i.e. the matching name without varia
 """
 struct Label{T}
     label::T
+
+    Label(label::T) where {T} = new{T}(label)
 end
 
+Label(label::Label) = label
 Label(name::String) = Label(Symbol(name))
+
+Base.show(io::IO, label::Label) = print(io, label.label)
 
 """
     Variance
@@ -25,9 +30,12 @@ end
 
 Represents an index of a tensor. It consists of a [`Label`](@ref) and [`Variance`](@ref) information.
 """
-struct Index{T}
-    label::T
-    variance::Variance
+Base.@kwdef struct Index{T}
+    label::Label{T}
+    variance::Variance = Invariant
+
+    Index(label::T, variance::Variance = Invariant) where {T} = new{T}(Label(label), variance)
+    Index(label::Label{T}, variance::Variance = Invariant) where {T} = new{T}(label, variance)
 end
 
 Index(ind::Index, variance::Variance = variance(ind)) = Index(label(ind), variance)
@@ -37,6 +45,8 @@ Base.copy(x::Index) = x
 
 label(ind::Index) = ind.label
 variance(ind::Index) = ind.variance
+
+variate(ind::Index, var::Variance) = Index(label(ind), var)
 
 is_equal_label(ind::Index) = Base.Fix1(is_equal_label, ind)
 is_equal_label(ind1::Index, ind2::Index) = label(ind1) == label(ind2)
