bb3d9b1b6417d431f9902737a841e9023bb59eea
Sergio Sánchez Ramírez <mofeing@gmail.com>
2026-06-03 10:56:22 -0500

    format test

--- test/integration/omeinsum.jl
+++ test/integration/omeinsum.jl
@@ -2,74 +2,72 @@ using Test
 using Muscle: Muscle, binary_einsum
 using OMEinsum
 
-@testset "binary_einsum" begin
-    old = Muscle.getbackend(binary_einsum, Muscle.PlatformHost())
-    Muscle.setbackend!(binary_einsum, Muscle.PlatformHost(), Muscle.BackendOMEinsum())
+old = Muscle.getbackend(binary_einsum, Muscle.PlatformHost())
+Muscle.setbackend!(binary_einsum, Muscle.PlatformHost(), Muscle.BackendOMEinsum())
 
-    @testset "matmul: ij,jk->ik" begin
-        a = ones(2, 3)
-        b = ones(3, 4)
-        c = binary_einsum(a, b; contracting_dims=[[2], [1]])
-        @test c == 3 * ones(2, 4)
-    end
-
-    @testset "inner product: ij,ji->" begin
-        a = ones(3, 4)
-        b = ones(4, 3)
-        c = binary_einsum(a, b; contracting_dims=[[2, 1], [1, 2]])
-        @test c == fill(12)
-    end
-
-    @testset "outer product: ij,kl->ijkl" begin
-        a = ones(2, 3)
-        b = ones(4, 5)
-        c = binary_einsum(a, b; contracting_dims=[Int[], Int[]])
-        @test c == fill(1, 2, 3, 4, 5)
-    end
-
-    @testset "scale: ij,->ij" begin
-        a = ones(2, 3)
-        α = fill(2.0)
-
-        let c = binary_einsum(a, α; contracting_dims=((), ()))
-            @test c == α[] .* a
-        end
-
-        let c = binary_einsum(α, a; contracting_dims=((), ()))
-            @test c == α[] .* a
-        end
-    end
-
-    @testset "batch matmul: ijb,jkb->ikb" begin
-        a = ones(2, 3, 6)
-        b = ones(3, 4, 6)
-        c = binary_einsum(a, b; contracting_dims=[[2], [1]], batching_dims=[[3], [3]])
-        @test c ≈ fill(3.0, 6, 2, 4)
-    end
-
-    @testset "manual" begin
-        @testset "eltype = $T" for T in [Float64, ComplexF64]
-            a = ones(T, 2, 3, 4)
-            b = ones(T, 4, 5, 3)
-
-            # contraction of all common indices
-            @testset "ijk,klj->il" begin
-                c = binary_einsum(a, b; contracting_dims=[[2, 3], [3, 1]])
-                @test c ≈ begin
-                    a_mat = reshape(a, 2, 12)
-                    b_mat = reshape(permutedims(b, [3, 1, 2]), 12, 5)
-                    a_mat * b_mat
-                end
-            end
-
-            # contraction of not all common indices
-            # hyperindices not supported on backendbase
-            @testset "ijk,klj->ikl" begin
-                c = binary_einsum(a, b; contracting_dims=[[2], [3]], batching_dims=[[3], [1]])
-                @test c ≈ fill(T(3.0), 4, 2, 5)
-            end
-        end
-    end
-
-    Muscle.setbackend!(binary_einsum, Muscle.PlatformHost(), old)
+@testset "matmul: ij,jk->ik" begin
+    a = ones(2, 3)
+    b = ones(3, 4)
+    c = binary_einsum(a, b; contracting_dims=[[2], [1]])
+    @test c == 3 * ones(2, 4)
 end
+
+@testset "inner product: ij,ji->" begin
+    a = ones(3, 4)
+    b = ones(4, 3)
+    c = binary_einsum(a, b; contracting_dims=[[2, 1], [1, 2]])
+    @test c == fill(12)
+end
+
+@testset "outer product: ij,kl->ijkl" begin
+    a = ones(2, 3)
+    b = ones(4, 5)
+    c = binary_einsum(a, b; contracting_dims=[Int[], Int[]])
+    @test c == fill(1, 2, 3, 4, 5)
+end
+
+@testset "scale: ij,->ij" begin
+    a = ones(2, 3)
+    α = fill(2.0)
+
+    let c = binary_einsum(a, α; contracting_dims=((), ()))
+        @test c == α[] .* a
+    end
+
+    let c = binary_einsum(α, a; contracting_dims=((), ()))
+        @test c == α[] .* a
+    end
+end
+
+@testset "batch matmul: ijb,jkb->ikb" begin
+    a = ones(2, 3, 6)
+    b = ones(3, 4, 6)
+    c = binary_einsum(a, b; contracting_dims=[[2], [1]], batching_dims=[[3], [3]])
+    @test c ≈ fill(3.0, 6, 2, 4)
+end
+
+@testset "manual" begin
+    @testset "eltype = $T" for T in [Float64, ComplexF64]
+        a = ones(T, 2, 3, 4)
+        b = ones(T, 4, 5, 3)
+
+        # contraction of all common indices
+        @testset "ijk,klj->il" begin
+            c = binary_einsum(a, b; contracting_dims=[[2, 3], [3, 1]])
+            @test c ≈ begin
+                a_mat = reshape(a, 2, 12)
+                b_mat = reshape(permutedims(b, [3, 1, 2]), 12, 5)
+                a_mat * b_mat
+            end
+        end
+
+        # contraction of not all common indices
+        # hyperindices not supported on backendbase
+        @testset "ijk,klj->ikl" begin
+            c = binary_einsum(a, b; contracting_dims=[[2], [3]], batching_dims=[[3], [1]])
+            @test c ≈ fill(T(3.0), 4, 2, 5)
+        end
+    end
+end
+
+Muscle.setbackend!(binary_einsum, Muscle.PlatformHost(), old)
