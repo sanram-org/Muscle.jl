b93281b3693097531cbf3f5b8455a26cfdbd4c5f
Sergio Sanchez Ramirez <15837247+mofeing@users.noreply.github.com>
2026-06-03 12:48:55 +0200

    update VectorInterface integration

--- ext/MuscleVectorInterfaceExt.jl
+++ ext/MuscleVectorInterfaceExt.jl
@@ -4,9 +4,9 @@ using Muscle
 using VectorInterface
 
 VectorInterface.scalartype(::Tensor{T}) where {T} = T
-VectorInterface.zerovector(t::Tensor, S) = Tensor(zerovector(parent(t), S), inds(t))
+VectorInterface.zerovector(t::Tensor, S) = Tensor(zerovector(parent(t), S), variance(t))
 VectorInterface.zerovector!(t::Tensor) = t .= zero(eltype(t))
-VectorInterface.scale(x::Tensor, α) = Tensor(scale(parent(x), α), inds(x))
+VectorInterface.scale(x::Tensor, α) = Tensor(scale(parent(x), α), variance(x))
 VectorInterface.scale!(x::Tensor, α) = x .*= α
 VectorInterface.scale!(y::Tensor, x::Tensor, α) = y .= α .* x
 VectorInterface.add(y::Tensor, x::Tensor) = y + x
@@ -16,9 +16,6 @@ VectorInterface.add!(y::Tensor, x::Tensor) = y .= y + x
 VectorInterface.add!(y::Tensor, x::Tensor, α::Number) = y .= y + α * x
 VectorInterface.add!(y::Tensor, x::Tensor, α::Number, β::Number) = y .= y * β + α * x
 
-function VectorInterface.inner(x::Tensor, y::Tensor)
-    @assert issetequal(inds(x), inds(y))
-    return binary_einsum(x, y)
-end
+VectorInterface.inner(x::Tensor, y::Tensor) = binary_einsum(x, y; contracting_dims=[collect(1:ndims(x)), collect(1:ndims(y))])
 
 end
