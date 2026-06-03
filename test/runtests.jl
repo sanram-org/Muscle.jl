d6ff33c10f597e9370a23550c5625947ef457b2f
Sergio Sanchez Ramirez <15837247+mofeing@users.noreply.github.com>
2026-06-03 13:42:00 +0200

    update CI workflow

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,30 +2,36 @@ using Muscle
 using Test
 using SafeTestsets
 
-@testset "Unit" verbose = true begin
-    @testset "Variance" include("core/variance.jl")
-    @testset "Operations" verbose = true begin
-        @testset "binary_einsum" include("core/operations/binary_einsum.jl")
-        @testset "tensor_qr" include("core/operations/tensor_qr.jl")
-        @testset "tensor_svd" include("core/operations/tensor_svd.jl")
-        @testset "tensor_eigen" include("core/operations/tensor_eigen.jl")
-        @testset "simple_update" include("core/operations/simple_update.jl")
+target_tests = isempty(ARGS) ? ["core", "integration"] : ARGS
+
+if "core" in target_testsets
+    @testset "Unit" verbose = true begin
+        @testset "Variance" include("core/variance.jl")
+        @testset "Operations" verbose = true begin
+            @testset "binary_einsum" include("core/operations/binary_einsum.jl")
+            @testset "tensor_qr" include("core/operations/tensor_qr.jl")
+            @testset "tensor_svd" include("core/operations/tensor_svd.jl")
+            @testset "tensor_eigen" include("core/operations/tensor_eigen.jl")
+            @testset "simple_update" include("core/operations/simple_update.jl")
+        end
+        @testset "Tensor" include("core/tensor.jl")
     end
-    @testset "Tensor" include("core/tensor.jl")
 end
 
-@testset "Integration" verbose = true begin
-    @safetestset "OMEinsum" include("integration/omeinsum.jl")
-    @safetestset "Strided" include("integration/strided.jl")
-    @safetestset "Dagger" include("integration/dagger.jl")
-
-    if !isnothing(get(ENV, "MUSCLE_TEST_CUDA", nothing))
-        @safetestset "CUDA" include("integration/cuda.jl")
+if "integration" in target_testsets
+    @testset "Integration" verbose = true begin
+        @safetestset "OMEinsum" include("integration/omeinsum.jl")
+        @safetestset "Strided" include("integration/strided.jl")
+        @safetestset "Dagger" include("integration/dagger.jl")
     end
+end
 
-    if !isnothing(get(ENV, "MUSCLE_TEST_REACTANT", nothing))
-        @safetestset "Reactant" include("integration/reactant.jl")
-    end
+if "cuda" in target_testsets
+    @safetestset "CUDA" include("integration/cuda.jl")
+end
+
+if "reactant" in target_testsets
+    @safetestset "Reactant" include("integration/reactant.jl")
 end
 
 # using Aqua
