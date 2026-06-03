f7e7f41ec93fb040ece1956067c1e5a2c8cd8dca
Sergio Sanchez Ramirez <15837247+mofeing@users.noreply.github.com>
2026-06-03 14:18:49 +0200

    fix test

--- test/core/variance.jl
+++ test/core/variance.jl
@@ -6,6 +6,6 @@ using Muscle: factordims
 @test Covariant' == Contravariant
 @test Contravariant' == Covariant
 
-@test factordims([Covariant, Contravariant]) == ([1], [2])
-@test factordims([Contravariant, Covariant]) == ([2], [1])
-@test factordims([Covariant, Contravariant, Covariant, Contravariant]) == ([1, 3], [2, 4])
+@test factordims([Covariant, Contravariant]) == ([2], [1])
+@test factordims([Contravariant, Covariant]) == ([1], [2])
+@test factordims([Covariant, Contravariant, Covariant, Contravariant]) == ([2, 4], [1, 3])
